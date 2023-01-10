e198bb45d3b1c26580785a936b0e45d3bd424560
gabrielmacieldev <gabrieljosephmaciel@gmail.com>
2023-01-10 19:17:43 -0300

    atualizado até aprofundamento express

--- build/index.js
+++ build/index.js
@@ -1,6 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const database_1 = require("./database");
+const cors_1 = __importDefault(require("cors"));
+const express_1 = __importDefault(require("express"));
 console.log("Hello world!");
 console.log("Usuário Cadastrado");
 console.table(database_1.users);
@@ -12,4 +17,13 @@ console.table((0, database_1.queryProductsByName)("plAca De víDeo"));
 console.table((0, database_1.queryProductsByName)("FUBá"));
 console.table((0, database_1.getAllPurchasesFromUserId)("a02"));
 console.table((0, database_1.getAllPurchasesFromUserId)("a01"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+app.listen(8080, () => {
+    console.log('servidor rodando na porta 8080');
+});
+app.get('/ping', (req, res) => {
+    res.send('pong');
+});
 //# sourceMappingURL=index.js.map
